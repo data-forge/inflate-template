5c31852d9012fb9dbb960a2e129f7d44c2da94e3
Ashley Davis <ashley@codecapers.com.au>
2018-08-06 15:01:35 +1000

    First pass of export library.

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,169 @@
+import * as globby from 'globby';
+import * as path from 'path';
+const promisify = require('promisify-any');
+import * as fs from 'fs-extra';
+import * as Handlebars from 'handlebars';
 
-import * as moment from 'moment';
+/**
+ * Represents an expanded file in a template.
+ */
+export interface ITemplateFile {
+    /**
+     * Relative path of the file within the tempalte folder..
+     */
+    relativePath: string;
 
-export class ExampleClass {
+    /**
+     * Expand the files content filling in gaps with data.
+     */
+    /*async*/ expand(): Promise<string>;
 
-    test(): void {
-        console.log("Test: " + moment().format('YYYY-DD-MM'));
-    }
-
-    returnsTrue(): boolean {
-        return true;
-    }
-
+    /**
+     * Expand and output the file to the output path.
+     * 
+     * @param outputPath The path to output the file to.
+     */
+    /*async*/ export(outputPath: string): Promise<void>;
 }
 
-//TODO: Code for your reusable code module goes here.
+//
+// Represents an expanded file in a template.
+//
+class TemplateFile implements ITemplateFile {
+
+    //
+    // Data injected into the template.
+    //
+    private data: any;
+
+    //
+    // Options to configure the template/export. 
+    //
+    private options: IExportOptions;
+
+    //
+    // Full path to the file.
+    //
+    private fullPath: string;
+
+    /**
+     * The name of the file.
+     */
+    relativePath: string;
+
+    constructor(data: any, options: IExportOptions, filePath: string, templatePath: string) {
+        this.data = data;
+        this.options = options;
+        this.fullPath = filePath;
+        this.relativePath = path.relative(templatePath, filePath);
+    }
+
+    /**
+     * Expand the files content filling in gaps with data.
+     */
+    async expand(): Promise<string> {
+        const templateData = await promisify(fs.readFile)(this.fullPath, 'utf8');
+        const expandedTemplate = Handlebars.compile(templateData)(this.data);
+        return expandedTemplate;
+    }
+
+    /**
+     * Expand and output the file to the output path.
+     * 
+     * @param outputPath The path to output the file to.
+     */
+    async export(outputPath: string): Promise<void> {
+        const expandedContent = this.expand();
+        await fs.ensureDir(outputPath);
+        await promisify(fs.writeFile)(path.join(outputPath, this.relativePath), expandedContent);
+    }
+}
+
+
+/**
+ * Represents an inflated template.
+ */
+export interface ITemplate {
+    /**
+     * Files contained in the inflated template.
+     */
+    files: ITemplateFile[];
+}
+
+//
+// Represents an inflated template.
+//
+class Template implements ITemplate {
+    //
+    // Data injected into the template.
+    //
+    private data: any;
+
+    //
+    // Options to configure the template/export. 
+    //
+    private options: IExportOptions;
+
+    //
+    // Files contained in the inflated template.
+    //
+    files: ITemplateFile[] = [];
+
+    constructor(data: any, options: IExportOptions) {
+        this.data = data;
+        this.options = options;
+    }
+
+    //
+    // Read the file system and determine the files in the template.
+    //
+    async readFiles(): Promise<void> {
+        const templateFileWildcard = path.join(this.options.templatePath, "**/*");
+        const templateFilePaths = await globby(templateFileWildcard);
+        this.files = templateFilePaths
+            .map(templateFilePath => 
+                new TemplateFile(
+                    this.data, 
+                    this.options, 
+                    templateFilePath,
+                    this.options.templatePath,
+                )
+            );
+    }
+}
+
+export interface IExportOptions {
+    //
+    // The path that contains the template to expand.
+    //
+    templatePath: string;
+}
+
+/**
+ * Inflate a template in memory.
+ * 
+ * @param data The data to expand the template.
+ * @param options Various options.
+ * 
+ * @returns An inflated template.
+ */
+export async function inflateTemplate(data: any, options: IExportOptions): Promise<ITemplate> {
+    const template = new Template(data, options);
+    await template.readFiles();
+    return template;
+}
+
+/**
+ * Do a full export. Inflate the specified template with data and write all expanded files to the
+ * specified output directory.
+ * 
+ * @param data The data to expand the template.
+ * @param outputPath The path to output expanded files to.
+ * @param options Various options.
+ */
+export async function exportTemplate (data: any, outputPath: string, options: IExportOptions): Promise<void> {
+    const template = await inflateTemplate(data, options);
+    for (const file of template.files) {
+        await file.export(outputPath);
+    }   
+}
--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,13 +1,116 @@
 import { assert, expect } from 'chai';
 import 'mocha';
-import { ExampleClass } from '../index';
+import { inflateTemplate, exportTemplate } from '../index';
 
-describe('main test suite', () => {
+const mock = require('mock-fs');
 
-    it('should be true', ()  => {
+//test:
+//  - can expand single file
+//  - can expand multiple files
+//  - can expand files in nested sub-dir
+//  - can expand files in memory only
 
-        expect((new ExampleClass()).returnsTrue()).to.be.true;      
+describe('export', () => {
 
+    afterEach(() => {
+        mock.restore();
     });
 
+    it('can inflate one file in memory', async ()  => {
+
+        const testFileContent = "some test content!!";
+
+        mock({
+            "c:/test/my-template": {
+                "some-file.txt": testFileContent,
+            },
+        });        
+
+        const data = {};
+        const options = {
+            templatePath: "c:/test/my-template",
+        };
+
+        const template = await inflateTemplate(data, options);
+        expect(template.files.length).to.eql(1);
+        expect(template.files[0].relativePath).to.eql("some-file.txt");
+        
+        const fileContent = await template.files[0].expand();
+        expect(fileContent).to.eql(testFileContent);
+    });
+
+    it('can inflate multiple files in memory', async ()  => {
+
+        const testFileContent1 = "test 1";
+        const testFileContent2 = "test 2";
+
+        mock({
+            "c:/test/my-template": {
+                "some-file-1.txt": testFileContent1,
+                "some-file-2.txt": testFileContent2,
+            },
+        });        
+
+        const data = {};
+        const options = {
+            templatePath: "c:/test/my-template",
+        };
+
+        const template = await inflateTemplate(data, options);
+        expect(template.files.length).to.eql(2);
+        expect(template.files[0].relativePath).to.eql("some-file-1.txt");
+        expect(template.files[1].relativePath).to.eql("some-file-2.txt");
+        
+        const fileContent1 = await template.files[0].expand();
+        expect(fileContent1).to.eql(testFileContent1);
+
+        const fileContent2 = await template.files[1].expand();
+        expect(fileContent2).to.eql(testFileContent2);
+    });
+
+    it('can inflate nested files in memory', async ()  => {
+
+        const testFileContent = "nested file content";
+
+        mock({
+            "c:/test/a-template": {
+                "some-dir": {
+                    "some-nested-file.txt": testFileContent,
+                },
+            },
+        });        
+
+        const data = {};
+        const options = {
+            templatePath: "c:/test/a-template",
+        };
+
+        const template = await inflateTemplate(data, options);
+        expect(template.files.length).to.eql(1);
+        expect(template.files[0].relativePath).to.eql("some-dir\\some-nested-file.txt");
+        
+        const fileContent1 = await template.files[0].expand();
+        expect(fileContent1).to.eql(testFileContent);
+    });
+
+    it('can expand template', async ()  => {
+
+        const testFileContent = "some {{fooey}} content!!";
+
+        mock({
+            "c:/test/my-template": {
+                "some-file.txt": testFileContent,
+            },
+        });        
+
+        const data = { fooey: 'excellent' };
+        const options = {
+            templatePath: "c:/test/my-template",
+        };
+
+        const template = await inflateTemplate(data, options);
+        expect(template.files.length).to.eql(1);
+        const fileContent = await template.files[0].expand();
+        expect(fileContent).to.eql("some excellent content!!");
+    });
 });
